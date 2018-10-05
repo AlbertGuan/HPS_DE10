e6424e25e8b60cb58806e404d246976babf622cc
Albert Guan <aoboguanhust@gmail.com>
2018-10-04 21:48:52 -0700

    Added LCD ctrl; Formatted the code base

---
+++ LCD_Display.h
@@ -0,0 +1,13 @@
+/*
+ * LCD_Display.h
+ *
+ *  Created on: Oct 4, 2018
+ *      Author: aobog
+ */
+
+#ifndef LCD_DISPLAY_H_
+#define LCD_DISPLAY_H_
+
+int LCD_Display();
+
+#endif /* LCD_DISPLAY_H_ */
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "LEDCtrl.h"
 #include "GSensor.h"
 #include "I2CSwitch.h"
+#include "LCD_Display.h"
 
 int main(int argc, char **argv)
 {
@@ -8,6 +9,8 @@ int main(int argc, char **argv)
 
 	//GSensor(argc, argv);
 
-	i2c_switch(argc, argv);
+	//i2c_switch(argc, argv);
+
+	LCD_Display();
 	return 0;
 }
